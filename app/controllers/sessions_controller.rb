037175027592bf1be6fa013c75d81005ae49ed15
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-12-25 09:31:47 +0000

    [add]comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    #入力したメールを持つユーザーの存在の確認及びパスワードの可否。あればtrueになる。
     if user && user.authenticate(params[:session][:password])
+      #ユーザーを登録したと同時にログイン
       log_in user
       redirect_to user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,21 @@
 module SessionsHelper
-
+  #セッションコントローラー渡されているユーザーでログイン
   def log_in(user)
     session[:user_id] = user.id
   end
 
   def current_user
+    #ログイン中の場合、現在ログインしているユーザーを返す
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
+    #ユーザーがログインしていればtrue,していなければfalse
     !current_user.nil?
   end
 
   def log_out
+    #現在のユーザーをログアウト
     session.delete(:user_id)
     @current_user = nil
   end
